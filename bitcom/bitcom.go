375fda6cdf005277b0dd10607985492801b19eca
David Case <david.case@shruggr.cloud>
2025-03-18 17:12:11 +0000

    update bitcom

--- bitcom/bitcom.go
+++ bitcom/bitcom.go
@@ -3,37 +3,59 @@ package bitcom
 import "github.com/bsv-blockchain/go-sdk/script"
 
 type Bitcom struct {
-	Protocol string
-	Script   []byte
-	Pos      int
+	Protocols    []*BitcomProtocol `json:"protos"`
+	ScriptPrefix []byte            `json:"prefix,omitempty"`
+}
+type BitcomProtocol struct {
+	Protocol string `json:"proto"`
+	Script   []byte `json:"script"`
+	Pos      int    `json:"pos"`
 }
 
-func Decode(scr *script.Script) (bitcoms []*Bitcom) {
+func Decode(scr *script.Script) (bitcom *Bitcom) {
 	pos := findReturn(scr, 0)
 	if pos == -1 {
 		return
+	}
+	bitcom = &Bitcom{
+		ScriptPrefix: (*scr)[:pos-1],
 	}
 	pos++
 
 	for {
 		pipePos := findPipe(scr, pos)
-		bitcom := &Bitcom{
+		p := &BitcomProtocol{
 			Pos: pos,
 		}
 		if op, err := scr.ReadOp(&pos); err != nil {
 			return
 		} else {
-			bitcom.Protocol = string(op.Data)
+			p.Protocol = string(op.Data)
 		}
 		if pipePos == -1 {
-			bitcom.Script = (*scr)[pos:]
-			bitcoms = append(bitcoms, bitcom)
-			return bitcoms
+			p.Script = (*scr)[pos:]
+			bitcom.Protocols = append(bitcom.Protocols, p)
+			return bitcom
 		}
-		bitcom.Script = (*scr)[pos:]
-		bitcoms = append(bitcoms, bitcom)
+		p.Script = (*scr)[pos:]
+		bitcom.Protocols = append(bitcom.Protocols, p)
 		pos = pipePos + 2
 	}
+}
+
+func (b *Bitcom) Lock() *script.Script {
+	s := script.NewFromBytes(b.ScriptPrefix)
+	if len(b.Protocols) > 0 {
+		s.AppendOpcodes(script.OpRETURN)
+		for i, p := range b.Protocols {
+			s.AppendPushData([]byte(p.Protocol))
+			s.AppendPushData(p.Script)
+			if i < len(b.Protocols)-1 {
+				s.AppendPushData([]byte("|"))
+			}
+		}
+	}
+	return s
 }
 
 func findReturn(scr *script.Script, pos int) int {
